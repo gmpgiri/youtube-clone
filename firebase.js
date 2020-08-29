5fd4d54b6e3cf7776e00829b5115716ef2ca273d
Giri Madhan <gmpgiri@gmail.com>
2020-08-29 10:27:42 +0530

    Initialize project using Create React App

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBJ81YG59DcD0HB7iHwGKeTttMASqGI5sE",
+  authDomain: "clone-562cf.firebaseapp.com",
+  databaseURL: "https://clone-562cf.firebaseio.com",
+  projectId: "clone-562cf",
+  storageBucket: "clone-562cf.appspot.com",
+  messagingSenderId: "381661077130",
+  appId: "1:381661077130:web:c788fa601b002e2dc5d773",
+  measurementId: "G-1JVX6ZGYLH",
+};
