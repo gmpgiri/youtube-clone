80e4725cd6222c460652b8a97a4ab7ba66c6f3fe
Giri Madhan <gmpgiri@gmail.com>
2020-08-30 11:07:33 +0530

    sidebar done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
-import "./App.css";
 import Header from "./Header";
-
+import Sidebar from "./Sidebar";
+import RecommendedVideos from "./RecommendedVideos";
+import "./App.css";
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* Sidebar */}
-      {/* Recommended Videos */}
+      <div className="app__page">
+        <Sidebar />
+        <RecommendedVideos />
+      </div>
     </div>
   );
 }
