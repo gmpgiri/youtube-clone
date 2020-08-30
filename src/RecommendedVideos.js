54cda0039cb6f6d150822d528821db9a88e8895a
Giri Madhan <gmpgiri@gmail.com>
2020-08-30 13:00:16 +0530

    Video card added

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,9 +1,78 @@
 import React from "react";
 import "./RecommendedVideos.css";
+import VideoCard from "./VideoCard";
 const RecommendedVideos = () => {
   return (
     <div className="recommendedVideos">
-      <h1>This is where videos will go</h1>
+      <h1>Recommended</h1>
+      <div className="recommendedVideos__videos">
+        <VideoCard
+          title="Rakithe Rakithe Rakithe "
+          image="https://i.ytimg.com/an_webp/5UmaMK_OfPw/mqdefault_6s.webp?du=3000&sqp=CIySrfoF&rs=AOn4CLDolIJesHPbNkShDw12ivInPTtHpg"
+          views="2.4M Views"
+          timestamp=" 2 days ago"
+          channel="Sony Music South"
+          channelImage="https://yt3.ggpht.com/a-/AOh14GhrJPMTBlx46qGWS-9N4JavrfUZchBe3kwN8w=s68-c-k-c0x00ffffff-no-rj-mo"
+        />
+        <VideoCard
+          title="Which is the Best State in India? | Dhruv Rathee Analysis on Economy, Environment, Development"
+          image="https://i.ytimg.com/an_webp/XPwVq56l1rk/mqdefault_6s_480x270.webp?du=3000&sqp=CKiPrfoF&rs=AOn4CLBxJA3KnYA9AiTkIy7BaQqzdSueng"
+          views="2.4M Views"
+          timestamp=" 2 days ago"
+          channel="Dhruv Rathee"
+          channelImage="https://yt3.ggpht.com/a-/AOh14GhUZ-JkdOzPCPyWavVVvXn79nr3SIhoppKC1A=s68-c-k-c0x00ffffff-no-rj-mo"
+        />
+        <VideoCard
+          title="How I Type REALLY Fast (156 Words per Minute)"
+          image="https://i.ytimg.com/an_webp/1ArVtCQqQRE/mqdefault_6s.webp?du=3000&sqp=CNySrfoF&rs=AOn4CLAc4IhEZHFlnqo5mUSE-xsakqSQdQ"
+          views="2.4M Views"
+          timestamp=" 2 days ago"
+          channel="Ali Abdaal
+          "
+          channelImage="https://yt3.ggpht.com/a/AATXAJzY2Kppv3nT5iG-DUjbr9wYCkw55-OOgcRUOykI=s88-c-k-c0xffffffff-no-rj-mo"
+        />
+        <VideoCard
+          title="Carryminati | Youtube vs Tiktok by Dhruv Rathee          "
+          image="https://i.ytimg.com/an_webp/9NJP5d0zuiY/mqdefault_6s.webp?du=3000&sqp=CJ7urPoF&rs=AOn4CLBnemZkQh02dQaYYceWZ5wOoT4grQ"
+          views="2.4M Views"
+          timestamp=" 2 days ago"
+          channel="Dhruv Rathee"
+          channelImage="https://yt3.ggpht.com/a-/AOh14GhUZ-JkdOzPCPyWavVVvXn79nr3SIhoppKC1A=s68-c-k-c0x00ffffff-no-rj-mo"
+        />
+        <VideoCard
+          title="Rakithe Rakithe Rakithe "
+          image="https://i.ytimg.com/an_webp/5UmaMK_OfPw/mqdefault_6s.webp?du=3000&sqp=CIySrfoF&rs=AOn4CLDolIJesHPbNkShDw12ivInPTtHpg"
+          views="2.4M Views"
+          timestamp=" 2 days ago"
+          channel="Sony Music South"
+          channelImage="https://yt3.ggpht.com/a-/AOh14GhrJPMTBlx46qGWS-9N4JavrfUZchBe3kwN8w=s68-c-k-c0x00ffffff-no-rj-mo"
+        />
+        <VideoCard
+          title="Which is the Best State in India? | Dhruv Rathee Analysis on Economy, Environment, Development"
+          image="https://i.ytimg.com/an_webp/XPwVq56l1rk/mqdefault_6s_480x270.webp?du=3000&sqp=CKiPrfoF&rs=AOn4CLBxJA3KnYA9AiTkIy7BaQqzdSueng"
+          views="2.4M Views"
+          timestamp=" 2 days ago"
+          channel="Dhruv Rathee"
+          channelImage="https://yt3.ggpht.com/a-/AOh14GhUZ-JkdOzPCPyWavVVvXn79nr3SIhoppKC1A=s68-c-k-c0x00ffffff-no-rj-mo"
+        />
+        <VideoCard
+          title="How I Type REALLY Fast (156 Words per Minute)"
+          image="https://i.ytimg.com/an_webp/1ArVtCQqQRE/mqdefault_6s.webp?du=3000&sqp=CNySrfoF&rs=AOn4CLAc4IhEZHFlnqo5mUSE-xsakqSQdQ"
+          views="2.4M Views"
+          timestamp=" 2 days ago"
+          channel="Ali Abdaal
+        "
+          channelImage="https://yt3.ggpht.com/a/AATXAJzY2Kppv3nT5iG-DUjbr9wYCkw55-OOgcRUOykI=s88-c-k-c0xffffffff-no-rj-mo"
+        />
+        <VideoCard
+          title="Carryminati | Youtube vs Tiktok by Dhruv Rathee          "
+          image="https://i.ytimg.com/an_webp/9NJP5d0zuiY/mqdefault_6s.webp?du=3000&sqp=CJ7urPoF&rs=AOn4CLBnemZkQh02dQaYYceWZ5wOoT4grQ"
+          views="2.4M Views"
+          timestamp=" 2 days ago"
+          channel="Dhruv Rathee"
+          channelImage="https://yt3.ggpht.com/a-/AOh14GhUZ-JkdOzPCPyWavVVvXn79nr3SIhoppKC1A=s68-c-k-c0x00ffffff-no-rj-mo"
+        />
+      </div>
     </div>
   );
 };
