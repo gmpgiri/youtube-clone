64064bd157d51a2405b2fb3af11bebae34c03bdc
Giri Madhan <gmpgiri@gmail.com>
2020-08-31 23:11:32 +0530

    working on search page

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
 import VideoCallIcon from "@material-ui/icons/VideoCall";
@@ -6,21 +6,33 @@ import AppsIcon from "@material-ui/icons/Apps";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import { Avatar } from "@material-ui/core";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [inputSearch, setInputSearch] = useState("");
+
   return (
     <div className="header">
       <div className="header__left">
         <MenuIcon />
-        <img
-          className="header__logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/753px-Logo_of_YouTube_%282015-2017%29.svg.png"
-          alt="logo"
-        />
+        <Link to="/">
+          <img
+            className="header__logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/753px-Logo_of_YouTube_%282015-2017%29.svg.png"
+            alt="logo"
+          />
+        </Link>
       </div>
       <div className="header__input">
-        <input placeholder="Search" type="text" />
-        <SearchIcon className="header__inputButton" />
+        <input
+          value={inputSearch}
+          onChange={(e) => setInputSearch(e.target.value)}
+          placeholder="Search"
+          type="text"
+        />
+        <Link to={`/search/${inputSearch}`}>
+          <SearchIcon className="header__inputButton" />
+        </Link>
       </div>
       <div className="header__icons">
         <VideoCallIcon className="header__icon" />
---
+++ src/SearchPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./SearchPage.css";
+import TuneIcon from "@material-ui/icons/Tune";
+const SearchPage = () => {
+  return (
+    <div className="searchPage">
+      <div className="searchPage__filter">
+        <TuneIcon />
+        <h2>FILTER</h2>
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default SearchPage;
